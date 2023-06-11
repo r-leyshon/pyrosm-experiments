51626fb1763aa282ab6e4a91206356569549cc98
r-leyshon <richard.leyshon@ons.gov.uk>
2023-06-11 10:49:27 +0100

    feat: App ingests nets & writes to disk

---
+++ pyrosm-cities-app/01-update-db.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import os
+import toml
+
+import pyrosm
+from pyrosm.data import sources
+from pyprojroot import here
+from django.utils.text import slugify
+
+"""This script is used to generate or overwrite a database of
+pyrosm-derived features for use with the pyrosm-cities-app. This is to
+avoid the demanding processing load that causes out of memory error on
+shinyapps.io.
+"""
+# configure script
+CONF = toml.load(here("pyrosm-cities-app/config/01-update-db.toml"))
+AOI = CONF["cities"]["aoi"]
+MODES = CONF["network"]["modes"]
+# find osm available cities & compare with AOI
+cities = [x.lower() for x in sources.cities.available]
+missing = set(AOI).difference(cities)
+if len(missing) > 0:
+    raise ValueError(
+        f"Cities being searched for are not in pyrosm.sources:\n{', '.join(missing)}"
+    )
+
+# ingest the data to tmp - don't store as too large, create osm objects
+osm_cities = dict()
+for city in AOI:
+    fp = pyrosm.get_data(city)
+    osm_cities[city] = pyrosm.OSM(fp)
+
+# ingest the available networks & write to disk
+out_pth = here("pyrosm-cities-app/data/")
+Path.mkdir(out_pth)  # handles missing parent dirs
+n_nets = len(osm_cities) * len(MODES)
+n = 1
+for city, osm in osm_cities.items():
+    for mod in MODES:
+        print(f"Extracting network {n} of {n_nets}")
+        net = osm.get_network(network_type=mod)
+        n += 1
+        # slugify standardises filenames
+        slug = slugify(f"{city}-net-{mod}")
+        fname = os.path.join(out_pth, f"{slug}.arrow")
+        print(f"Writing net to {fname}")
+        net.to_feather(fname)
