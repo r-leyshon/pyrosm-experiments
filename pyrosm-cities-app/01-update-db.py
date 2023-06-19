657490fc1e96253612cc2bb185a764f56ab9e41f
r-leyshon <richard.leyshon@ons.gov.uk>
2023-06-19 16:36:31 +0100

    feat: Add control flow for handling cities unavailable in pyrosm sources

--- pyrosm-cities-app/01-update-db.py
+++ pyrosm-cities-app/01-update-db.py
@@ -3,9 +3,11 @@ import os
 import toml
 from datetime import datetime
 import re
+import subprocess
 
 import pyrosm
 from pyrosm.data import sources
+from pyrosm_proto import DecodeError
 from pyprojroot import here
 from django.utils.text import slugify
 import numpy as np
@@ -19,20 +21,10 @@ shinyapps.io.
 CONF = toml.load(here("pyrosm-cities-app/config/01-update-db.toml"))
 AOI = CONF["cities"]["aoi"]
 MODES = CONF["network"]["modes"]
+BBOXES = CONF["osm"]["bbox"]
+REGIONS = CONF["osm"]["region"]
 # find osm available cities & compare with AOI
 cities = [x.lower() for x in sources.cities.available]
-missing = set(AOI).difference(cities)
-if len(missing) > 0:
-    raise ValueError(
-        f"Cities being searched for are not in pyrosm.sources:\n{', '.join(missing)}"
-    )
-
-# ingest the data to tmp - don't store as too large, create osm objects
-osm_cities = dict()
-for city in AOI:
-    fp = pyrosm.get_data(city)
-    osm_cities[city] = pyrosm.OSM(fp)
-
 # extract the available networks & write to disk
 out_pth = here("pyrosm-cities-app/data/")
 try:
@@ -41,25 +33,8 @@ except FileExistsError:
     pass
 # date for vintages
 vint = datetime.strftime(datetime.now(), "%Y-%m-%d")
-n_cities = len(osm_cities)
-n_nets = n_cities * len(MODES)
-n = 1
-for city, osm in osm_cities.items():
-    for mod in MODES:
-        print(f"Extracting network {n} of {n_nets}")
-        net = osm.get_network(network_type=mod)
-        n += 1
-        # slugify standardises filenames
-        slug = slugify(f"{city}-net-{mod}-{vint}")
-        fname = os.path.join(out_pth, f"{slug}.arrow")
-        # retain only columns of interest
-        net = net.loc[:, ["geometry", "length", "maxspeed"]]
-        print(f"Writing net to {fname}")
-        net.to_feather(fname)
 
-
-# extract land use features & write to disk
-n = 1
+# ingest the data to tmp - don't store as too large, create osm objects
 transp = "|".join(["aero", "railway", "highway", "motorway", "road", "runway"]).lower()
 
 agri = "|".join(
@@ -140,6 +115,80 @@ indus_pat = re.compile(indus, re.IGNORECASE)
 amen_pat = re.compile(amen, re.IGNORECASE)
 dev_pat = re.compile(develop, re.IGNORECASE)
 
+rock_pat = re.compile(
+    r"rock|cliff|shingle|sand|stone|scree|gorge|ridge|landslide|mountain", re.IGNORECASE
+)
+water_pat = re.compile(
+    r"bay|beach|coast|spring|water|wet|shoal|river|flood|reed", re.IGNORECASE
+)
+green_pat = re.compile(
+    r"grass|mud|heath|tree|shrub|scrub|scub|wood|forest|field|earth|meadow|lawn|fell",
+    re.IGNORECASE,
+)
+
+osm_cities = dict()
+for city in AOI:
+    if city in cities:
+        # logic to ingest city data from pyrosm
+        pass
+        fp = pyrosm.get_data(city)
+    else:
+        # logic to ingest region data with pyrosm, then use osmium to filter to bbox
+        # get the OSM for the region
+        region = REGIONS[city]
+        bbox = BBOXES[city]
+        bbox_str = ",".join([str(x) for x in bbox])
+        print(f"City not available in pyrosm sources. Ingesting from {region} region.")
+        fp = pyrosm.get_data(region)
+        tmp = os.path.dirname(fp)
+        out_tmp = os.path.join(tmp, f"{slugify(city)}-osm.pbf")
+        subprocess.run(
+            [
+                "osmium",
+                "extract",
+                "--strategy",
+                "complete_ways",
+                "--bbox",
+                bbox_str,
+                fp,
+                "-o",
+                out_tmp,
+            ]
+        )
+
+    osm_cities[city] = pyrosm.OSM(fp)
+
+
+n_cities = len(osm_cities)
+n_nets = n_cities * len(MODES)
+n = 1
+probs = []
+# road network
+for city, osm in osm_cities.items():
+    for mod in MODES:
+        print(f"Extracting network {n} of {n_nets}")
+        try:
+            net = osm.get_network(network_type=mod)
+            # slugify standardises filenames
+            slug = slugify(f"{city}-net-{mod}-{vint}")
+            fname = os.path.join(out_pth, f"{slug}.arrow")
+            # retain only columns of interest
+            net = net.loc[:, ["geometry", "length", "maxspeed"]]
+            print(f"Writing net to {fname}")
+            net.to_feather(fname)
+        except DecodeError:
+            print(f"OSM encoding problem encountered with {city}. Skipping.")
+            probs.append(city)
+            pass
+        n += 1
+
+# remove any city that threw a decode error
+for prob in probs:
+    del osm_cities[prob]
+    n_cities -= 1
+
+# extract land use features & write to disk
+n = 1
 for city, osm in osm_cities.items():
     print(f"Extracting landuse features {n} of {n_cities}")
     landuse = osm.get_landuse()
@@ -185,13 +234,6 @@ for city, osm in osm_cities.items():
 
 # finally with green space
 n = 1
-rock_pat = re.compile(
-    r"rock|cliff|shingle|sand|stone|scree|gorge|ridge|landslide|mountain"
-)
-water_pat = re.compile(r"bay|beach|coast|spring|water|wet|shoal|river|flood|reed")
-green_pat = re.compile(
-    r"grass|mud|heath|tree|shrub|scrub|scub|wood|forest|field|earth|meadow|lawn|fell"
-)
 for city, osm in osm_cities.items():
     print(f"Extracting natural features {n} of {n_cities}")
     nat = osm.get_natural()
